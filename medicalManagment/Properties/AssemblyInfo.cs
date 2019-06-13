90ac17b63b58abed2956ceba22b246b276ab796f
abdallah <abdallah@example.com>
2019-06-13 05:19:32 +0200

    final version

--- medicalManagment/Properties/AssemblyInfo.cs
+++ medicalManagment/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dentist")]
+[assembly: AssemblyTitle("medicalManagment")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("dentist")]
+[assembly: AssemblyProduct("medicalManagment")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
